f60231eab1ce83f193a4e09071d30e6b7364c393
Shp1nat <stac987653210@mail.ru>
2024-03-29 04:08:49 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace _4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(56, 64);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(400, 400);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(1014, 64);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(400, 400);
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Загрузить изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(646, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Размытие";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(646, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 44);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Бинаризация";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(646, 191);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(132, 44);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Контуры";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(646, 312);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 44);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Примитивы";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(804, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(815, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 2;
+            this.trackBar1.Location = new System.Drawing.Point(631, 80);
+            this.trackBar1.Maximum = 99;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(167, 45);
+            this.trackBar1.SmallChange = 2;
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.Value = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 2;
+            this.trackBar2.Location = new System.Drawing.Point(631, 249);
+            this.trackBar2.Maximum = 99;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(167, 45);
+            this.trackBar2.SmallChange = 2;
+            this.trackBar2.TabIndex = 12;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(631, 362);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(167, 45);
+            this.trackBar3.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(818, 379);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1471, 640);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.ML.MlEnum;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace _4
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> sourceImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Изображения (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    sourceImage = new Image<Bgr, byte>(ofd.FileName);
+                }
+                imageBox1.Image = sourceImage.Resize(400, 400, Inter.Linear);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var blurImage = blur(sourceImage.Clone(), trackBar1.Value);
+            imageBox2.Image = blurImage.Resize(400, 400, Inter.Linear);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var blurImage = blur(sourceImage.Clone(), trackBar1.Value);
+            var binarizedImage = bimarize(blurImage, trackBar2.Value);
+            imageBox2.Image = binarizedImage.Resize(400, 400, Inter.Linear);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var blurImage = blur(sourceImage.Clone(), trackBar2.Value);
+            var binarizedImage = bimarize(blurImage, trackBar1.Value);
+            var contours = findCounters(binarizedImage);
+            var contoursImageBlank = drawCounters(sourceImage.CopyBlank(), contours);
+            imageBox2.Image = contoursImageBlank.Resize(400, 400, Inter.Linear);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var blurImage = blur(sourceImage.Clone(), trackBar2.Value);
+            var binarizedImage = bimarize(blurImage, trackBar1.Value);
+            var contours = findCounters(binarizedImage);
+            var contoursImageBlank = drawCounters(sourceImage.CopyBlank(), contours);
+            var approxedBorders = primitivesFind(contoursImageBlank.CopyBlank(), contours, trackBar3.Value);
+            var numberOfPrimitives = primitives(contoursImageBlank.CopyBlank(), contours, trackBar3.Value);
+            imageBox2.Image = approxedBorders.Resize(400, 400, Inter.Linear);
+            textBox1.Text = "" + numberOfPrimitives;
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label1.Text = String.Format("{0}", trackBar1.Value);
+        }
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = String.Format("{0}", trackBar2.Value);
+        }
+        public Image<Gray, byte> blur(Image<Bgr, byte> _sourceImage, int kernelSize)
+        {
+            var grayImage = _sourceImage.Convert<Gray, byte>();
+            var bluredImage = grayImage.SmoothGaussian(kernelSize);
+            return bluredImage;
+        }
+        public Image<Gray, byte> bimarize(Image<Gray, byte> _sourceImage, int binarizationThreshold)
+        {
+            var threshold = new Gray(80);
+            var color = new Gray(255);
+            var binarizedImage = _sourceImage.ThresholdBinary(threshold, color);
+            return binarizedImage;
+        }
+        public VectorOfVectorOfPoint findCounters(Image<Gray, byte> binImg)
+        {
+            var contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(binImg, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+            return contours;
+        }
+        public Image<Bgr, byte> drawCounters(Image<Bgr, byte> dstImage, VectorOfVectorOfPoint borders)
+        {
+            for (int i = 0; i < borders.Size; i++)
+            {
+                var points = borders[i].ToArray();
+                dstImage.Draw(points, new Bgr(Color.GreenYellow), 2);
+            }
+            return dstImage;
+        }
+        public Image<Bgr, byte> primitivesFind(Image<Bgr, byte> contImg, VectorOfVectorOfPoint borders, double minArea)
+        {
+            for (int i = 0; i < borders.Size; i++)
+            {
+                var approxContour = new VectorOfPoint();
+                CvInvoke.ApproxPolyDP(borders[i], approxContour, CvInvoke.ArcLength(borders[i], true) * 0.05, true);
+                if (CvInvoke.ContourArea(approxContour, false) > minArea)
+                {
+                    if (approxContour.Size == 3)
+                    {
+                        var points = approxContour.ToArray();
+                        contImg.Draw(new Triangle2DF(points[0], points[1], points[2]), new Bgr(Color.GreenYellow), 2);
+                    }
+                }
+            }
+            return contImg;
+        }
+        public int primitives(Image<Bgr, byte> contImg, VectorOfVectorOfPoint borders, double min)
+        {
+            int count = 0;
+            for (int i = 0; i < borders.Size; i++)
+            {
+                var approxContour = new VectorOfPoint();
+                CvInvoke.ApproxPolyDP(borders[i], approxContour, CvInvoke.ArcLength(borders[i], true) * 0.05, true);
+                if (CvInvoke.ContourArea(approxContour, false) > min)
+                {
+                    if (approxContour.Size == 3)
+                    {
+                        var points = approxContour.ToArray();
+                        contImg.Draw(new Triangle2DF(points[0], points[1], points[2]), new Bgr(Color.GreenYellow), 2);
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+
+    
+}
